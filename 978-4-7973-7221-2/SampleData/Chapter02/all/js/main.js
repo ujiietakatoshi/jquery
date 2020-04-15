56632ba097269b1ff80dbcec3367f7c980a015c2
ujiietakatoshi <fadeoutinu@gmail.com>
2020-04-15 21:13:55 +0900

    commit3

--- 978-4-7973-7221-2/SampleData/Chapter02/all/js/main.js
+++ 978-4-7973-7221-2/SampleData/Chapter02/all/js/main.js
@@ -1,4 +1,14 @@
 $(function(){
-  $('#typo').css('color','#ebc000');
-
+  $('#typo').on('mouseover',function(){
+    $('#typo').css({
+      color:'#ebc000',
+      backgroundColor:'#ae5e9b'
+  });
+});
+  $('#typo').on('mouseout',function(){
+    $('#typo').css({
+      color:'',
+      backgroundColor:''
+   });
+ });
 });
